0f5562c6c88bdab8fb11996e8e75bbf020cfd710
Nightjuanes <juanrodgon@unisabana.edu.co>
2025-11-14 18:36:02 -0500

    Implementacion de funciones BLOQUEAR Y GANAR
    en este codigo pusimos primero una regla que va ser que la primera ficha porque conozco el juego lo mejor es comenzar en el centro, si esta ocupado lo mejor es la opcion es la segunda, Ademas implemente funciones para si tiene la oprtunidad de tirar una ficha y ganar lo haga, como segunda opcion que bloquee para no perder. Ya si no hay mas la tiraria aleatoria

--- tournament/groups/Group B/policy.py
+++ tournament/groups/Group B/policy.py
@@ -1,16 +1,55 @@
 import numpy as np
 from connect4.policy import Policy
+from connect4.connect_state import ConnectState
 from typing import override
 
-
-class Hello(Policy):
+class juanesagente(Policy):
 
     @override
     def mount(self) -> None:
-        pass
+        self.move_count = 0
 
     @override
     def act(self, s: np.ndarray) -> int:
-        rng = np.random.default_rng()
-        available_cols = [c for c in range(7) if s[0, c] == 0]
-        return int(rng.choice(available_cols))
+        estado = ConnectState(board=s)
+        available_cols = estado.get_free_cols()
+        
+        # jugada segun experiencia
+        if self.move_count == 0:
+            if 3 in available_cols:
+                self.move_count += 1
+                return 3
+            else :
+                self.move_count += 1
+                return 1  
+        
+        # funcion de si puede ganar jugando en esa fila que la retorne
+        def puede_ganar(estado: ConnectState, col: int) -> int:
+            nuevo_estado = estado.transition(col)
+            if nuevo_estado.is_final() and nuevo_estado.get_winner() == estado.player:
+                self.move_count += 1
+                return col
+        
+        #funcion que bloquee si el oponente puede ganar en la siguiente jugada
+        def bloquear_ganar_oponente(estado: ConnectState, col: int) -> int:
+            nuevo_estado = estado.transition(col)
+            opponent_cols = nuevo_estado.get_free_cols()
+            for opp_col in opponent_cols:
+                opp_estado = nuevo_estado.transition(opp_col)
+                if opp_estado.is_final() and opp_estado.get_winner() == -estado.player:
+                    self.move_count += 1
+                    return col  # Bloquea
+        #aplicar ambas funciones
+        for col in available_cols:
+            resultado = puede_ganar(estado, col)
+            if resultado is not None:
+                return resultado
+        
+        for col in available_cols:
+            resultado = bloquear_ganar_oponente(estado, col)
+            if resultado is not None:
+                return resultado
+                
+        # Si no hay jugadas estratégicas, elige aleatoriamente
+        self.move_count += 1
+        return np.random.choice(available_cols)
