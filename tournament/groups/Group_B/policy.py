a1963e133ef463aabb98b790f7717200b079097b
nataliaocmo <jerg2003@gmail.com>
2025-11-24 13:37:48 -0500

    Update policy.py

--- tournament/groups/Group_B/policy.py
+++ tournament/groups/Group_B/policy.py
@@ -6,6 +6,81 @@ from connect4.policy import Policy
 from connect4.connect_state import ConnectState
 
 
+class MCTS:
+    def __init__(self, state: ConnectState, parent=None, action=None):
+        self.state = state                  
+        self.parent = parent                 
+        self.action = action                 
+        self.children = []                  
+        self.visits = 0                       
+        self.wins = 0                       
+        self.untried_actions = self.get_actions() 
+
+    def get_actions(self):
+         return self.state.get_free_cols()
+
+    def is_terminal(self):
+        return self.state.is_final()
+
+    def all_actions_tried(self):
+        return len(self.untried_actions) == 0
+
+    def check_winner(self):
+        return self.state.get_winner()
+
+    def expand(self):
+        action = self.untried_actions.pop()
+        new_state = self.state.transition(action)
+        child = MCTS(new_state, parent=self, action=action)
+        self.children.append(child)
+        return child
+
+    def best_child(self, c=1.4):
+        return max(self.children, key=lambda child:
+                   (child.wins / child.visits) +
+                   c * math.sqrt(math.log(self.visits) / child.visits))
+
+    def rollout(self):
+        current_state = self.state
+        initial_player = self.state.player
+        while not current_state.is_final():
+            actions = current_state.get_free_cols()
+            if not actions:
+                return 0  
+            action = random.choice(actions)
+            current_state = current_state.transition(action)
+        winner = current_state.get_winner()
+        if winner == initial_player:
+            return 1.0
+        elif winner == 0:
+            return 0
+        else:
+            return -1.0
+
+    def backpropagate(self, result):
+        self.visits += 1
+        self.wins += result
+        if self.parent:
+            self.parent.backpropagate(result)
+
+
+def mcts_search(root_state: ConnectState, iterations=100):
+    root = MCTS(root_state)
+    for iteration in range(iterations):
+        node = root
+        # Seleccion
+        while not node.is_terminal() and node.all_actions_tried():
+            node = node.best_child()
+        # Expansion de un nodo hijo
+        if not node.is_terminal():
+            node = node.expand()
+        # Simulatcion
+        result = node.rollout()
+        # transferencia de resultados
+        node.backpropagate(result)
+    return root.best_child(c=0).action  
+
+
 class juanes_agente(Policy):
     def __init__(self):
         self.episodio_completo = []
